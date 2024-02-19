062df2eb12d514f743572a03566537672b8c692d
NandayGB <fernandagbarbosa97@gmail.com>
2024-02-19 18:40:50 -0300

    Conta bancária

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -1,10 +1,25 @@
 package conta;
 
+import conta.model.conta;
+
 public class Menu {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+
+		conta c1 = new conta(1, 123, 1, " Amanda", 25000.00f);
+
+		c1.visualizar();
+		c1.setAgencia(456);
+		System.out.println(c1.getAgencia());
+
+		if (c1.sacar(100))
+			;
+		System.out.println(" Saque efetuado com sucesso.O novo saldo é de R$: " + c1.getSaldo());
+	
+		
+		c1.depositar(1000);
+		System.out.println("O novo saldo é: " + c1.getSaldo());
 	}
 
 }
