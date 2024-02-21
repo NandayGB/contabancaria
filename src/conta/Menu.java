f3897d1ef5c50c08cd165d6b1ecaf01f499b22de
NandayGB <fernandagbarbosa97@gmail.com>
2024-02-20 22:48:58 -0300

    primeiro commit no novo branch

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -1,6 +1,10 @@
 package conta;
 
 import conta.model.conta;
+import conta.model.ContaCorrente;
+import conta.model.ContaPoupanca;
+
+
 
 public class Menu {
 
@@ -20,6 +24,26 @@ public class Menu {
 		
 		c1.depositar(1000);
 		System.out.println("O novo saldo é: " + c1.getSaldo());
+		c1.visualizar();
+		System.out.println("O  saldo é: " + c1.getSaldo());
+	
+		ContaCorrente cc1 = new ContaCorrente(1, 678, 1, "Anna Julia", 0.0f, 1000.0f);
+		cc1.visualizar();
+		cc1.sacar(12000.0f);
+		cc1.visualizar();
+		cc1.depositar(5000.0f);
+		cc1.visualizar();
+		
+		ContaPoupanca cp1 = new ContaPoupanca(2, 123, 2, "Julia", 100000.0f, 15);
+		cp1.visualizar();
+        cp1.sacar(1000.0f);
+		cp1.visualizar();
+		cp1.depositar(5000.0f);
+		cp1.visualizar();
+		
 	}
 
 }
+
+
+
