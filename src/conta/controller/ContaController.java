d144f34a5ce5d79c23ef7dc5d1bd6b07a60f7537
NandayGB <fernandagbarbosa97@gmail.com>
2024-02-23 16:27:13 -0300

    terceiro commit no novo branch

---
+++ src/conta/controller/ContaController.java
@@ -0,0 +1,19 @@
+package conta.controller;
+
+import java.util.ArrayList;
+
+import conta.model.conta;
+import conta.repository.ContaRepository;
+
+
+public class ContaController implements ContaRepository {
+
+	
+	private ArrayList<conta> listacontas = new ArrayList <conta> ();
+	int numero = 0 ;
+	
+	
+
+	}
+
+}
